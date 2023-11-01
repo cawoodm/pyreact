adcdffebd73cc579808077f64a84dbe5153dbc6f
Marc Cawood <498834+cawoodm@users.noreply.github.com>
2023-11-01 18:24:04 +0000

    CORS backend

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 Demo of Python backend with React frontend
 
 ## Creation
+
+### Frontend
 ```bash
 npm create vite@latest
 # React, TypeScript, frontend
 cd frontend
 npm i
 npm run dev
+```
+
+### Backend
+```bash
+# Install flask
+pip install flask
 ```
--- backend/website/views.py
+++ backend/website/views.py
@@ -3,13 +3,16 @@ from flask import send_from_directory
 
 views = Blueprint('views', __name__)
 
+# Host built UI in backend
+
 
 @views.route('/')
 @views.route('/<path:path>')
 def index(path='index.html'):
-    return send_from_directory('dist', path)
+    return send_from_directory('dist', path), {"Access-Control-Allow-Origin": "*"}
 
 
-@views.route('/shopping-list')
+@views.route('/api/shopping-list')
 def todos():
-    return ['Grapes', 'Soap', 'Spaghetti', 'Marmalade', 'Bread']
+
+    return ['Grapes', 'Soap', 'Spaghetti', 'Marmalade', 'Bread'], {"Access-Control-Allow-Origin": "*"}
